39cab41b50bb97e2d1f8081d3fec25dd3e5fbdc7
郭小凡 <glider.guo@ankobot.com>
2022-09-22 18:33:50 +0800

    全部lesson编译通过;

--- docs/record.md
+++
@@ -1,2 +0,0 @@
-* [ ] catkin_create 和catkin_make 的区别;
-* [ ] ros不容许有中文路径,只能换到主目录下
--- install_dependence.sh
+++ install_dependence.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
+###
+ # @Author: glider
+ # @Date: 2022-09-21 19:31:47
+ # @LastEditTime: 2022-09-22 18:32:49
+ # @FilePath: /SubstrateDevelopment/modules-3rd/Creating-2D-laser-slam-from-scratch/install_dependence.sh
+ # @Version:  v0.01
+ # @Description: 
+ # ************************************************************************
+ # Copyright (c) 2022 by  glider.guo@ankobot.com, All Rights Reserved. 
+ # ************************************************************************
+### 
 
 # lesson3
+# sudo apt-get install ros-kinetic-csm
 sudo apt-get install ros-kinetic-csm
-
 # lesson4
 sudo apt-get install ros-kinetic-laser-geometry
 
@@ -26,3 +37,4 @@ mkdir build
 cd build
 cmake .. 
 make
+make  install
